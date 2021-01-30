09b289c79d155ca5bfba74db2f659cf5c418982e
itzomen <peng.akebuon2468@gmail.com>
2021-01-30 02:17:57 +0100

    created updatedb.cpp to update database value

---
+++ Tests/updatedb.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <pqxx/pqxx> 
+
+using namespace std;
+using namespace pqxx;
+
+int main(int argc, char* argv[]) {
+   
+   try {
+      connection C("dbname = mydatabase user = eufresia password = password hostaddr = 127.0.0.1 port = 5432");
+      if (C.is_open()) {
+         cout << "Opened database successfully: " << C.dbname() << endl;
+      } else {
+         cout << "Can't open database" << endl;
+         return 1;
+      }
+      
+      /* Create a transactional object. */
+      work W(C);
+    
+      /* Execute SQL query */
+      W.exec( "UPDATE BOOK set AUTHOR = 'Omen' where ID=1" );
+      W.commit();
+      cout << "Records updated successfully" << endl;
+      
+
+      /* Create a non-transactional object. */
+      nontransaction N(C);
+      
+      /* Execute SQL query */
+      result R( N.exec( "SELECT * from BOOK"));
+      
+      /* List down all the records */
+      for (result::const_iterator c = R.begin(); c != R.end(); ++c) {
+         cout << "ID = " << c[0].as<int>() << endl;
+         cout << "Name = " << c[1].as<string>() << endl;
+         cout << "Author = " << c[2].as<string>() << endl;
+         cout << "Pub_name = " << c[3].as<string>() << endl;
+      }
+      cout << "Operation done successfully" << endl;
+      C.disconnect ();
+   } catch (const std::exception &e) {
+      cerr << e.what() << std::endl;
+      return 1;
+   }
+
+   return 0;
+}
